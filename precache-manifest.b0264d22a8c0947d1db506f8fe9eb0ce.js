46a1114836710edf037f1c403f8328ba57f94126
Agisilaos Kounelis <kounelisagis@gmail.com>
2021-10-24 13:42:15 +0300

    Updates

---
+++ precache-manifest.b0264d22a8c0947d1db506f8fe9eb0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "eac38289b59876194863",
+    "url": "/app/static/js/main.39d89faa.chunk.js"
+  },
+  {
+    "revision": "5c6c1fafd764ec75e833",
+    "url": "/app/static/js/2.e3f911e3.chunk.js"
+  },
+  {
+    "revision": "eac38289b59876194863",
+    "url": "/app/static/css/main.e3f2ca0d.chunk.css"
+  },
+  {
+    "revision": "5c6c1fafd764ec75e833",
+    "url": "/app/static/css/2.2f912659.chunk.css"
+  },
+  {
+    "revision": "a2833b2d373ee557e444080b124668e3",
+    "url": "/app/index.html"
+  }
+];
